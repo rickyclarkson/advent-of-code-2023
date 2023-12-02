98473da95f659f5b30a2d30f8bb7672f4c4a0e5c
Ricky Clarkson <ricky.clarkson@gmail.com>
2023-12-01 20:12:25 -0800

    Day 1 part 2

--- day1.kt
+++ day1.kt
@@ -7,13 +7,30 @@ fun main() {
   do {
     val line = reader.readLine()
     if (line == null) break
-    calibration += firstAndLastDigits(line) ?: continue
+    calibration += (calibrationBeginning(line) ?: continue) * 10
+    calibration += calibrationEnding(line) ?: continue
   } while (true)
   println(calibration)
 }
 
-fun firstAndLastDigits(line: String): Int? =
-  line.asIterable().filter { it.isDigit() }.let {
-    it.first().digitToInt() * 10 + it.last().digitToInt()
+fun calibrationBeginning(line: String): Int? {
+  for (x in 0..<line.length) {
+    if (line[x].isDigit()) return line[x].digitToInt()
+    for ((word, digit) in wordDigitPairs) {
+      if (line.substring(x).startsWith(word)) return digit
+    }
   }
+  return null
+}
 
+fun calibrationEnding(line: String): Int? {
+  for (x in line.length - 1 downTo 0) {
+    if (line[x].isDigit()) return line[x].digitToInt()
+    for ((word, digit) in wordDigitPairs) {
+      if (line.substring(x).startsWith(word)) return digit
+    }
+  }
+  return null
+}
+
+val wordDigitPairs = listOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
